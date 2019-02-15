8a8b0f1d5a969de9f412ac4eda97b6552d21f003
Simon O'Brien <simon@the-obriens.net>
2019-02-15 15:32:50 +0000

    Added support for session token

--- tasks/prepare-aws-test/task.sh
+++ tasks/prepare-aws-test/task.sh
@@ -10,8 +10,10 @@ terraform plan \
   -var "env_name=${ENV_NAME}" \
   -var "aws_access_key_id=${AWS_ACCESS_KEY_ID}" \
   -var "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" \
+  -var "aws_session_token=${AWS_SESSION_TOKEN}" \
   -var "access_key=${AWS_ACCESS_KEY_ID}" \
   -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
+  -var "session_token=${AWS_SESSION_TOKEN}" \
   -var "aws_region=${AWS_REGION}" \
   -var "region=${AWS_REGION}" \
   -out terraform.tfplan \
